2faeedcef1600121f9c2e9e968bb1caf50e29e25
Hendar02 <hendarpriana@gmail.com>
2021-01-20 12:26:10 +0700

    Piramida

---
+++ Piramida.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Nama			: Hendar Priana "<<endl;
+    cout<<"Kelas		: 1C "<<endl;
+    cout<<"NMP			: 20.14.1.0010 "<<endl;
+    cout<<endl;
+     for(int i=1 ; i<=20 ; i++){
+         for(int p=1 ; p<=i ; p++){
+             cout<<"  *";
+         }
+         cout << endl;
+      }
+      return 0;
+}
